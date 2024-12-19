2f193f85a07c8e1445078b093529960477ded808
Umang Rathod <umangrathoduk@gmail.com>
2024-12-19 23:30:34 +0000

    Initial commit to setup config server

---
+++ src/test/java/com/ecomm/configserver/EcommConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
